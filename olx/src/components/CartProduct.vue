54fab8140c3f49055392c701ab2b8e1382b2ffd0
Stanisław Bożych <stachubozych@gmail.com>
2022-10-26 00:00:41 +0200

    small bugfix
    due to last changes with database structure changed

--- olx/src/components/CartProduct.vue
+++ olx/src/components/CartProduct.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="wrapper">
+    <div class="wrapper" v-if="product.cartProducts">
         <div class="left">
             <img :src="product.cartProducts.src" alt="">
             <div class="data">
